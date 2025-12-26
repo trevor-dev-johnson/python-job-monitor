c5230fee612a11afb85e0bca61e7bf9941740f3c
Trevor Johnson <75464105+tJohnsonAce@users.noreply.github.com>
2025-12-26 14:27:09 -0500

    config update

--- config.py
+++ config.py
@@ -5,14 +5,15 @@ KEYWORDS = [
     "python",
     "software engineer",
     "api",
-    "platform"
+    "platform",
 ]
 
 EMAIL_FROM = os.getenv("EMAIL_FROM")
 EMAIL_TO = os.getenv("EMAIL_TO")
+
 SMTP_SERVER = os.getenv("SMTP_SERVER")
 SMTP_PORT = int(os.getenv("SMTP_PORT"))
 SMTP_PASSWORD = os.getenv("SMTP_PASSWORD")
 
-print("SMTP_SERVER =", SMTP_SERVER)
-print("SMTP_PORT =", SMTP_PORT)
+if not all([EMAIL_FROM, EMAIL_TO, SMTP_SERVER, SMTP_PORT, SMTP_PASSWORD]):
+    raise RuntimeError("Missing required email environment variables")
